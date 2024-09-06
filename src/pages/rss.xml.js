181279c99e66c34613617a6b3823cee91a116212
Alban Vigliano <vigliano.alban@gmail.com>
2024-09-06 14:35:32 +0200

    sixth commit RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 
 export async function GET(context) {
 	return rss({
-		title: "Astro Learner | Blog",
+		title: 'Astro Learner | Blog',
 		description: 'Mon voyage apprentissage Astro',
 		site: context.site,
 		items: await pagesGlobToRssItems(import.meta.glob("./**/*.md")),
