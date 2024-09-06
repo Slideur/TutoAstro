606f1b59d8a0832433333933a3c0546c508aafac
Alban Vigliano <vigliano.alban@gmail.com>
2024-09-06 14:26:55 +0200

    fourth commit RSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://tutoastrobyastro.netlify.app/",
+	site: "https://tutoastrobyastro.netlify.app/"
 });
