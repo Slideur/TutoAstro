ba7c6ae44af2965bc37d05db1201236d69a0b9bf
Alban Vigliano <vigliano.alban@gmail.com>
2024-09-09 13:51:46 +0200

    ninth commit RSS

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,10 +2,9 @@ import rss, { pagesGlobToRssItems } from "@astrojs/rss";
 
 export async function GET(context) {
 	return rss({
-		title: 'Astro Learner | Blog',
-		description: 'Mon voyage apprentissage Astro',
+		title: "Astro Learner | Blog",
+		description: "Mon voyage apprentissage Astro",
 		site: context.site,
-        items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
-          customData: `<language>fr-fr</language>`,
-        });
-      }
+		items: await pagesGlobToRssItems(import.meta.glob("./blog/*.{md,mdx}")),
+	});
+}
